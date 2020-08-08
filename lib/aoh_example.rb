3048d7c7d28f2ae4bbf0861fe2ec24a5a7ae2e1f
Conrad Buys <conradbuys11@gmail.com>
2020-08-08 19:35:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,10 +20,12 @@ def literal_aoh
     {name: "Joelle van Dyne", occupation: "Radio Personality"},
     {name:  "Pat Monteseian", occupation: "Staff"},
     {name:  "Kate Gompert", occupation: "None"},
-    {name:  "Bruce Green", occupation: "Fan of Mildred"}]
+    {name:  "Bruce Green", occupation: "Fan of Mildred"}
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
